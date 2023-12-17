f3a5a43f77166d73997dc5846d97fa2f665da597
ko-uema2 <ko-uema2@outlook.jp>
2023-12-17 22:26:22 +0900

    update countAndNotify フィルタ条件を更新に当初想定の実装を完成。

--- countAndNotifyNotionPagesToSlack/lambda/handler.ts
+++ countAndNotifyNotionPagesToSlack/lambda/handler.ts
@@ -12,9 +12,60 @@ const NOTION_DB_ID_KEY = process.env["NOTION_DB_ID"]!;
 const SLACK_BOT_TOKEN_KEY = process.env["SLACK_BOT_TOKEN"]!;
 const SLACK_CHANNEL_NAME_KEY = process.env["SLACK_CHANNEL_NAME"]!;
 
-const ssm = new SSMClient({ region: "ap-northeast-1" });
+export const handler = async (event: ScheduledEvent, context: Context) => {
+  const BEFORE_DAY = 7;
+  try {
+    const { notionAuth, notionDBId, slackBotToken, slackChannelName } =
+      await getParametersFromSSM();
+
+    if (!notionAuth || !notionDBId || !slackBotToken || !slackChannelName) {
+      throw new Error("必要な情報を全て取得できませんでした");
+    }
+
+    const targetDate = getBeforeDate(event.time, BEFORE_DAY);
+    console.log(`date: ${targetDate}`);
+
+    const notionClient = new Client({
+      auth: notionAuth,
+    });
+    const slackClient = new WebClient(slackBotToken);
+
+    const queryResult = await notionClient.databases.query({
+      database_id: notionDBId,
+      filter: {
+        and: [
+          {
+            property: "完了日",
+            date: {
+              on_or_after: targetDate,
+            },
+          },
+          {
+            property: "done",
+            checkbox: {
+              equals: true,
+            },
+          },
+        ],
+      },
+    });
+    const memoCount = queryResult.results.length;
+    const sendMsg = `先週は \`${memoCount}\` 件も隙間時間にやるべきことを消化した！\n隙間時間によく頑張った！ 感動したﾂ！！`;
+
+    await slackClient.chat.postMessage({
+      text: sendMsg,
+      channel: slackChannelName!,
+      mrkdwn: true,
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("エラーが発生しました", error);
+    }
+  }
+};
 
 const getParametersFromSSM = async () => {
+  const ssm = new SSMClient({ region: "ap-northeast-1" });
   const secureStrResponse = await ssm.send(
     new GetParametersCommand({
       Names: [NOTION_AUTH_KEY, NOTION_DB_ID_KEY, SLACK_BOT_TOKEN_KEY],
@@ -46,48 +97,13 @@ const getParametersFromSSM = async () => {
   };
 };
 
-export const handler = async (event: ScheduledEvent, context: Context) => {
-  try {
-    const { notionAuth, notionDBId, slackBotToken, slackChannelName } =
-      await getParametersFromSSM();
+const getBeforeDate = (dateStr: string, beforeDay: number): string => {
+  const dateObj = new Date(dateStr);
+  dateObj.setUTCDate(dateObj.getUTCDate() - beforeDay);
 
-    if (!notionAuth || !notionDBId || !slackBotToken || !slackChannelName) {
-      throw new Error("必要な情報を全て取得できませんでした");
-    }
-
-    const notionClient = new Client({
-      auth: notionAuth,
-    });
-    const slackClient = new WebClient(slackBotToken);
-
-    const queryResult = await notionClient.databases.query({
-      database_id: notionDBId,
-      filter: {
-        and: [
-          {
-            property: "GoodNotes",
-            status: {
-              equals: "メモあり",
-            },
-          },
-          {
-            property: "done",
-            checkbox: {
-              equals: false,
-            },
-          },
-        ],
-      },
-    });
-    const memoCount = queryResult.results.length.toString();
-
-    await slackClient.chat.postMessage({
-      text: memoCount,
-      channel: slackChannelName!,
-    });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.error("エラーが発生しました", error);
-    }
-  }
+  return new Intl.DateTimeFormat("ja-JP", {
+    timeZone: "Asia/Tokyo",
+  })
+    .format(dateObj)
+    .replace(/\//g, "-");
 };
